7bd7c0a3d8845d5307a5492f1591f673a3ca36d0
M4tthewDE <matthias.kronberg@gmail.com>
2024-04-04 16:45:05 +0200

    Move Property to class/mod.rs

--- src/kotlin/class/mod.rs
+++ src/kotlin/class/mod.rs
@@ -2,7 +2,7 @@ use anyhow::{bail, Context, Result};
 use tower_lsp::lsp_types::Hover;
 use tree_sitter::{Node, Tree};
 
-use self::function::{Function, Property};
+use self::function::Function;
 
 use super::Position;
 
@@ -14,6 +14,71 @@ pub enum ClassModifier {
     Visibility(String),
     Annotation(String),
     Inheritance(String),
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub enum PropertyModifier {
+    Annotation(String),
+    Member(String),
+    Visibility(String),
+    Inheritance(String),
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub struct Property {
+    pub modifiers: Vec<PropertyModifier>,
+    pub name: String,
+    // TODO: use DataType
+    pub type_identifier: Option<String>,
+}
+
+impl Property {
+    pub fn new(node: &Node, content: &[u8]) -> Result<Property> {
+        let mut modifiers: Vec<PropertyModifier> = Vec::new();
+        let mut cursor = node.walk();
+        for child in node.children(&mut cursor.clone()) {
+            if child.kind() == "modifiers" {
+                for child in child.children(&mut cursor) {
+                    match child.kind() {
+                        "annotation" => modifiers.push(PropertyModifier::Annotation(
+                            child.utf8_text(content)?.to_string(),
+                        )),
+                        "member_modifier" => modifiers.push(PropertyModifier::Member(
+                            child.utf8_text(content)?.to_string(),
+                        )),
+                        "visibility_modifier" => modifiers.push(PropertyModifier::Visibility(
+                            child.utf8_text(content)?.to_string(),
+                        )),
+                        "inheritance_modifier" => modifiers.push(PropertyModifier::Inheritance(
+                            child.utf8_text(content)?.to_string(),
+                        )),
+                        _ => bail!("unknown modifier {}", child.kind()),
+                    }
+                }
+            }
+
+            if child.kind() == "variable_declaration" {
+                let name = child
+                    .child(0)
+                    .context("no name found for variable declaration")?
+                    .utf8_text(content)?
+                    .to_string();
+                let type_identifier = if let Some(type_node) = child.child(2) {
+                    Some(type_node.utf8_text(content)?.to_string())
+                } else {
+                    None
+                };
+
+                return Ok(Property {
+                    modifiers,
+                    name,
+                    type_identifier,
+                });
+            }
+        }
+
+        bail!("no property found");
+    }
 }
 
 #[derive(Debug, Hash, PartialEq, Eq)]
