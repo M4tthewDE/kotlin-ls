4429dc34737f770c9e21fdc157b41b5091e590e6
M4tthewDE <matthias.kronberg@gmail.com>
2024-04-04 12:01:52 +0200

    Move class into module

--- src/kotlin/class.rs
+++ src/kotlin/class.rs
@@ -1,9 +1,5 @@
-use std::path::{Path, PathBuf};
-
 use anyhow::{bail, Context, Result};
-use tracing::warn;
-use tree_sitter::{Node, Parser, Tree};
-use walkdir::WalkDir;
+use tree_sitter::{Node, Tree};
 
 #[derive(Debug)]
 pub enum ClassModifier {
@@ -41,90 +37,7 @@ pub struct KotlinClass {
     pub body: ClassBody,
 }
 
-#[derive(Debug)]
-pub struct KotlinFile {
-    pub path: PathBuf,
-    pub package: String,
-    pub imports: Vec<String>,
-    pub classes: Vec<KotlinClass>,
-}
-
-impl KotlinFile {
-    fn new(path: PathBuf, tree: &Tree, content: &[u8]) -> Result<KotlinFile> {
-        let package = get_package(tree, content)?;
-        let imports = get_imports(tree, content)?;
-        let classes = get_classes(tree, content)?;
-
-        Ok(KotlinFile {
-            path,
-            package,
-            imports,
-            classes,
-        })
-    }
-}
-
-fn get_package(tree: &Tree, content: &[u8]) -> Result<String> {
-    let mut cursor = tree.walk();
-    loop {
-        let node = cursor.node();
-        if node.kind() == "package" {
-            return Ok(node
-                .next_sibling()
-                .context("no package found")?
-                .utf8_text(content)?
-                .to_string());
-        }
-
-        if cursor.goto_first_child() {
-            continue;
-        }
-
-        loop {
-            if cursor.goto_next_sibling() {
-                break;
-            }
-
-            if !cursor.goto_parent() {
-                bail!("no package found");
-            }
-        }
-    }
-}
-
-fn get_imports(tree: &Tree, content: &[u8]) -> Result<Vec<String>> {
-    let mut imports = Vec::new();
-    let mut cursor = tree.walk();
-    loop {
-        let node = cursor.node();
-        if node.kind() == "import" {
-            let import = node
-                .next_sibling()
-                .context("malformed import")?
-                .utf8_text(content)
-                .context("malformed import")?
-                .to_string();
-
-            imports.push(import);
-        }
-
-        if cursor.goto_first_child() {
-            continue;
-        }
-
-        loop {
-            if cursor.goto_next_sibling() {
-                break;
-            }
-
-            if !cursor.goto_parent() {
-                return Ok(imports);
-            }
-        }
-    }
-}
-
-fn get_classes(tree: &Tree, content: &[u8]) -> Result<Vec<KotlinClass>> {
+pub fn get_classes(tree: &Tree, content: &[u8]) -> Result<Vec<KotlinClass>> {
     let mut classes = Vec::new();
     let mut cursor = tree.walk();
     loop {
@@ -273,60 +186,4 @@ fn get_property(node: &Node, content: &[u8]) -> Result<Property> {
     }
 
     bail!("no property found");
-}
-
-pub struct KotlinProject {
-    pub files: Vec<KotlinFile>,
-}
-
-impl KotlinProject {
-    pub fn new(p: &Path) -> Result<KotlinProject> {
-        let mut files = Vec::new();
-        let mut parser = Parser::new();
-        parser.set_language(tree_sitter_kotlin::language()).unwrap();
-
-        for path in WalkDir::new(p)
-            .into_iter()
-            .filter_map(|e| e.ok())
-            .filter(|e| e.file_type().is_file())
-            .filter(|e| e.path().extension().map_or(false, |ext| ext == "kt"))
-            .map(|e| e.into_path())
-        {
-            let content = std::fs::read(&path).unwrap();
-            let tree = parser.parse(&content, None).unwrap();
-            match KotlinFile::new(path.clone(), &tree, &content) {
-                Ok(f) => files.push(f),
-                Err(err) => warn!("Failed to parse {:?}: {}", path, err),
-            }
-        }
-
-        Ok(KotlinProject { files })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::path::Path;
-
-    use crate::kotlin::KotlinProject;
-
-    #[test]
-    fn test_new() {
-        tracing_subscriber::fmt().init();
-        let p = Path::new("/home/matti/Programming/contributing/DankChat");
-
-        let project = KotlinProject::new(&p).unwrap();
-        for file in project.files {
-            if file
-                .path
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .contains("DankChatApplication.kt")
-            {
-                dbg!(file);
-            }
-        }
-    }
 }
