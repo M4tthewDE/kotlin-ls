d763b709e32d6d7772b870f0a51aae5ca446fcab
M4tthewDE <matthias.kronberg@gmail.com>
2024-04-09 18:32:54 +0200

    Implement for statement

--- src/kotlin/statement.rs
+++ src/kotlin/statement.rs
@@ -6,7 +6,14 @@ use super::{
     assignment::Assignment,
     expression::{ControlStructureBody, Expression, EXPRESSIONS},
     function::Function,
+    variable_declaration::{MultiVariableDeclaration, VariableDeclaration},
 };
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone)]
+pub enum ForParameter {
+    VariableDeclaration(VariableDeclaration),
+    MultiVariableDeclaration(MultiVariableDeclaration),
+}
 
 #[derive(Debug, Hash, PartialEq, Eq, Clone)]
 pub enum Statement {
@@ -15,6 +22,7 @@ pub enum Statement {
     Assignment(Assignment),
     Function(Function),
     While(Expression, Option<ControlStructureBody>),
+    For(Expression, ForParameter, Option<ControlStructureBody>),
 }
 
 pub fn get_statements(node: &Node, content: &[u8]) -> Result<Vec<Statement>> {
@@ -33,6 +41,7 @@ pub fn get_statements(node: &Node, content: &[u8]) -> Result<Vec<Statement>> {
                 statements.push(Statement::Assignment(Assignment::new(&child, content)?))
             }
             "while_statement" => statements.push(while_statement(&child, content)?),
+            "for_statement" => statements.push(for_statement(&child, content)?),
             kind => {
                 if EXPRESSIONS.contains(&kind) {
                     statements.push(Statement::Expression(Expression::new(&child, content)?))
@@ -78,5 +87,77 @@ fn while_statement(node: &Node, content: &[u8]) -> Result<Statement> {
         }
     } else {
         bail!("[Statement::While] no child at {}", node.start_position());
+    }
+}
+
+fn for_statement(node: &Node, content: &[u8]) -> Result<Statement> {
+    if let Some(last) = node.child(node.child_count() - 1) {
+        if last.kind() == ")" {
+            let child = node.child(node.child_count() - 5).context(format!(
+                "[Statement::For] no child at {}",
+                node.start_position()
+            ))?;
+            let parameter = match child.kind() {
+                "variable_declaration" => {
+                    ForParameter::VariableDeclaration(VariableDeclaration::new(&child, content)?)
+                }
+                "multi_variable_declaration" => ForParameter::MultiVariableDeclaration(
+                    MultiVariableDeclaration::new(&child, content)?,
+                ),
+                _ => {
+                    bail!(
+                        "[Statement::For] unhandled child {} '{}' at {}",
+                        child.kind(),
+                        child.utf8_text(content)?,
+                        child.start_position(),
+                    )
+                }
+            };
+            Ok(Statement::For(
+                Expression::new(
+                    &node.child(node.child_count() - 3).context(format!(
+                        "[Statement::For] no child at {}",
+                        node.start_position()
+                    ))?,
+                    content,
+                )?,
+                parameter,
+                None,
+            ))
+        } else {
+            let child = node.child(node.child_count() - 5).context(format!(
+                "[Statement::For] no child at {}",
+                node.start_position()
+            ))?;
+            let parameter = match child.kind() {
+                "variable_declaration" => {
+                    ForParameter::VariableDeclaration(VariableDeclaration::new(&child, content)?)
+                }
+                "multi_variable_declaration" => ForParameter::MultiVariableDeclaration(
+                    MultiVariableDeclaration::new(&child, content)?,
+                ),
+                _ => {
+                    bail!(
+                        "[Statement::For] unhandled child {} '{}' at {}",
+                        child.kind(),
+                        child.utf8_text(content)?,
+                        child.start_position(),
+                    )
+                }
+            };
+            Ok(Statement::For(
+                Expression::new(
+                    &node.child(node.child_count() - 3).context(format!(
+                        "[Statement::For] no child at {}",
+                        node.start_position()
+                    ))?,
+                    content,
+                )?,
+                parameter,
+                ControlStructureBody::new(&last, content).ok(),
+            ))
+        }
+    } else {
+        bail!("[Statement::For] no child at {}", node.start_position());
     }
 }
