298926ac4670213f2793fa92eaa3a80ed3c50d53
M4tthewDE <matthias.kronberg@gmail.com>
2024-04-11 10:38:22 +0200

    feat: remove indexed access from delegation parsing

--- src/kotlin/delegation.rs
+++ src/kotlin/delegation.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{bail, Result};
 use tree_sitter::Node;
 
 use super::{constructor_invocation::ConstructorInvocation, types::Type};
@@ -11,20 +11,30 @@ pub enum Delegation {
 
 impl Delegation {
     pub fn new(node: &Node, content: &[u8]) -> Result<Delegation> {
-        let child = node.child(0).context("no delegation specifier child")?;
-        match child.kind() {
-            "user_type" => Ok(Delegation::Type(Type::new(&child, content)?)),
-            "constructor_invocation" => Ok(Delegation::ConstructorInvocation(
-                ConstructorInvocation::new(&child, content)?,
-            )),
-            _ => {
-                bail!(
-                    "[Delegation] unhandled child {} '{}' at {}",
-                    child.kind(),
-                    child.utf8_text(content)?,
-                    child.start_position(),
-                )
+        let mut cursor = node.walk();
+        for child in node.children(&mut cursor) {
+            match child.kind() {
+                "line_comment" | "multi_line_comment" => {}
+                "user_type" => return Ok(Delegation::Type(Type::new(&child, content)?)),
+                "constructor_invocation" => {
+                    return Ok(Delegation::ConstructorInvocation(
+                        ConstructorInvocation::new(&child, content)?,
+                    ))
+                }
+                _ => {
+                    bail!(
+                        "[Delegation] unhandled child {} '{}' at {}",
+                        child.kind(),
+                        child.utf8_text(content)?,
+                        child.start_position(),
+                    )
+                }
             }
         }
+        bail!(
+            "[Delegation] no child at {} - {}",
+            node.start_position(),
+            node.end_position(),
+        )
     }
 }
